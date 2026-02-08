2d0d3a1dd6d6fad47953620ef30f22886e9f05f1
naiqi <Zhang.naiqi@hotmail.com>
2026-02-08 09:02:59 +0800

    MagPCGrad1.1
    没啥改的，修正了一些东西

--- train.py
+++ train.py
@@ -8,15 +8,15 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     # Build seg+pose model from yaml (no pretrained weights).
-    # model = YOLO("ultralytics/cfg/models/12/csp.yaml", task='segpose')
-    model = YOLO('runs/segment/magpc_csp_cos/weights/last.pt', task='segpose')
-    # model.load('yolov12n-seg.pt')
+    model = YOLO("ultralytics/cfg/models/12/csp.yaml", task='segpose')
+    # model = YOLO('runs/segment/magpc_csp_cos/weights/last.pt', task='segpose')
+    model.load('yolov12n-seg.pt')
 
     # Z:/datasets/segment/straw_segpose/straw-pose.yaml
 
     model.train(
         data="ultralytics/cfg/datasets/straw.yaml",
-        resume=True,
+        # resume=True,
         epochs=100,
         batch=64,nbs=64,
         workers=16,
@@ -24,6 +24,6 @@ if __name__ == "__main__":
         project='Z:/SegPose/SegPose/runs/segment',
         # grad_balance=None,
         grad_balance_shared=['body'],
-        name="magpc_csp_cos",
+        name="magpc-csp-cos-pretrain",
     )
 
